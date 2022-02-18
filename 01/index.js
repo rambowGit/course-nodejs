b85af0a92a05a3134078bafd4e7d3560c19033d9
Stepkin Alexander <stepkin.alexander@gmail.com>
2022-02-18 18:30:37 +0300

    home work 01

--- 01/index.js
+++ 01/index.js
@@ -3,80 +3,103 @@ const path = require('path');
  
 const baseDir = './music';
 const newDir =  './alphabetDirs'
-let filesToCopy={};
+
  
 /*
   Get files list
 */ 
-const readDir = (baseDir, tabLevel) => {
-  const files = fs.readdirSync(baseDir);
- 
-  files.forEach(item => {
-    let localBase = path.join(baseDir, item);
-    let state = fs.statSync(localBase);
-   
-    if (state.isDirectory()) {
-      console.log('-'.repeat(tabLevel) + ' DIR: ' + item);
-      readDir(localBase, tabLevel + 1);
-    } else {
-        // if object key exist, add fool file path to array
-        if (filesToCopy[item.charAt(0).toLowerCase()]) {
-          filesToCopy[item.charAt(0)].push(localBase);
-        } else {
-          filesToCopy[item.charAt(0).toLowerCase()] = [localBase];
-        }
-      console.log('-'.repeat(tabLevel) + ' File: ' + item);
-    }
-  })
+const lsDir = (baseDir) => {
+  let filesToCopy = {},
+  filesObject = {};  
+  
+   // curent not sorted  base dir 
+  fs.readdir(baseDir, (err, files) => {
+    if (err) {
+      console.error(err);
+      return;
+    } 
+    files.forEach(item => {
+        let localBase = path.join(baseDir, item);
+        fs.stat(localBase, (err, state) => {
+          if (err) {
+            console.error(err);
+            return;
+          } 
+          if (state.isDirectory()) {
+            lsDir(localBase);
+          } else {
+            // if object key exist, add fool file path to array
+            if (filesToCopy[item.charAt(0).toLowerCase()]) {
+              filesToCopy[item.charAt(0)].push(localBase);
+            } else {              
+              filesToCopy[item.charAt(0).toLowerCase()] = [localBase];
+            }
 
-  return Object.keys(filesToCopy).sort().reduce((r, k) => (r[k] = filesToCopy[k], r), {});
+            filesObject = Object.keys(filesToCopy).sort().reduce((r, k) => (r[k] = filesToCopy[k], r), {});
+            copyFiles(filesObject, newDir);
+          }
+        });     
+      })
+  });    
+  
 };
  
 /*
   Copy file to new dir
 */ 
 const copyFiles = (filesObj, targetDir) => { 
+
    const curentDir = process.cwd();
    const requiredDir = __dirname;   
    try {
      // check curent process location
-    if (curentDir == requiredDir) {
-      console.log('trying to create new dir');
-      if (!fs.existsSync(targetDir)){
-        fs.mkdirSync(targetDir);
-      }else {
-        console.log(`${targetDir} exist!`);
-       }
-     
-       // create new dirs
+    if (curentDir == requiredDir) {     
+       // create new sub-dirs
        for (let key in filesObj) {
         let newAlphabetDir = path.join(targetDir, key);
-        fs.mkdirSync(newAlphabetDir);
+        fs.mkdir(newAlphabetDir, (err) => {
+          if (err) {
+            if (err.code == 'EEXIST') {
+
+            }else console.error(err);
+          }
+          console.log(`Directory ${newAlphabetDir} created successfully!`);
+        });     
         // copy files to new dirs
-        filesObj[key].forEach(file => {
-          
+        filesObj[key].forEach(file => {          
           fs.link(file, path.join(newAlphabetDir, path.basename(file)), err => {
             if (err) {
               console.error(err.message);
               return;
             }
           });
-
         });
-      }
-      
-
-
-
+      }   
     } 
    } catch (err) {
     console.error('crete directory error: ', err); 
   }
+}
 
- 
-};
+/*
+ Create new base dir for alphabet sub-dirs
+*/
+
+const createNewBaseDir = (newBaseDir) => {
+  // create new base dir for sorted files
+  console.log('trying to create new base dir');
+  fs.mkdir(newBaseDir, err => {
+    if (err) {
+      if (err.code == 'EEXIST') {
+
+      }else console.error(err);
+    }
+  console.log(`Directory ${newBaseDir} created successfully!`);
+  }); 
+}
+
+// entry point
+createNewBaseDir(newDir);
+lsDir(baseDir);
 
 
-const filesObject = readDir(baseDir, 1);
-console.log("filesObject: ", filesObject);
-copyFiles(filesObject, newDir);
