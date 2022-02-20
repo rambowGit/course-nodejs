43b55d1c7c4b20ffd71754e6f6596b33091f9047
Stepkin Alexander <stepkin.alexander@gmail.com>
2022-02-20 20:06:59 +0300

    home work #2 done

--- httpServer/index.js
+++ httpServer/index.js
@@ -1,25 +1,33 @@
 const http = require('http');
+const timer = require('./timer.js')
 
 const port = 8080;
+// const timerInterval = 1000;
+// const timeToStop = 5000
+const timerInterval = process.env.TIMER_INTERVAL;
+const timeToStop = process.env.STOP_INTERVAL;
+
+if (!timerInterval || !timeToStop) {
+  console.log('TIMER_INTERVAL or STOP_INTERVAL not defined!');
+  return;
+}
 
 const requestListener = function (req, res) {
-  console.log(`Method: ${req.method}`);
-  console.log(`URL: ${req.url}`);
+ // console.log(`Method: ${req.method}`);
+ // console.log(`URL: ${req.url}`);
+
+ if (req.url == '/start') {
+  timer(timerInterval, timeToStop);
+ }
 
   res.writeHead(200);
   res.end('Hello, World!');
 }
 
-const server = http.createServer(requestListener);
+const server = http.createServer(requestListener)
+.on('error', (err) => console.log(err.message));
 
 server.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 })
-
-
-const timer = (interval) => {
-  const utcDate1 = new Date();
-  utcDate1.toUTCString()
-
-}
 
