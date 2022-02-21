c3a36b26507476e8850e4419d824be55788766b4
Stepkin Alexander <stepkin.alexander@gmail.com>
2022-02-21 15:09:08 +0300

    http server fixed

--- httpServer/index.js
+++ httpServer/index.js
@@ -3,7 +3,6 @@ const http = require('http');
 
 const port = 8080;
 let connections = [];
-const curDate = new Date();
 
 const timerInterval = process.env.TIMER_INTERVAL;
 const timeToStop = process.env.STOP_INTERVAL;
@@ -17,31 +16,30 @@ const requestListener = function (req, res) {
 
   connections.push(res);
 
-  if (req.url == '/start') {
-    res.setHeader('Content-Type', 'text/html');
-    res.writeHead(200);
-    res.write('Session started \n');
-    
-    // show date in browser
-    const showDate = setInterval(function(i) {      
-      res.write(`UTC time is: ${curDate.toUTCString()} \n`);
-    }, 
-    timerInterval);
-    
-    // iterating through different connections
-    connections.map(() => showDate );
-    
-    // stop setInterval
-    setTimeout(() => {
-                      if (timeToStop > 0) {
-                        clearInterval(showDate);
-                        res.write(`Disconected at  ${curDate.toUTCString()} \n`);
-                        res.end();
-                        connections = [];
-                      }                      
-                    }, timeToStop)
-    
-  } 
+  res.setHeader('Content-Type', 'text/html');
+  res.writeHead(200);
+  res.write('Session started \n');
+  
+  // show date in browser
+  const showDate = setInterval(function(i) {      
+    console.log(`UTC time is: ${new Date().toUTCString()}`);
+  }, 
+  timerInterval);
+  
+  // iterating through different connections
+  connections.map(() => showDate );
+  
+  // stop setInterval
+  setTimeout(() => {
+                    if (timeToStop > 0) {
+                      clearInterval(showDate);
+                      const curDate = new Date().toUTCString();
+                      res.write(`Disconected at  ${curDate} \n`);
+                      res.end();
+                      connections = [];
+                    }                      
+                  }, timeToStop)
+  
 }
 
 // run server
